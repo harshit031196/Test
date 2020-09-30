f4daee1466f65d74fae5d4b3b062a3e72da5e857
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-01 02:50:12 +0530

    Create userValidationException.java

---
+++ userValidateException/userValidationException.java
@@ -0,0 +1,12 @@
+
+public class userValidationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9011933370865976765L;
+	public userValidationException(String message) {
+		super(message);
+	}
+
+}
