bf9964aeb2f8ae4e762edae888d75800ca0a8879
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-01 02:54:58 +0530

    Create MoodException.java

---
+++ ExceptionTest/MoodException.java
@@ -0,0 +1,11 @@
+
+public class MoodException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public MoodException(String message) {
+		super(message);
+	}
+}
